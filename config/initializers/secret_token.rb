72351ce3f14da14bc1d6d685f0390fd569f260c6
Aniely Martins <aniellymartins@gmail.com>
2012-02-07 21:44:09 -0200

    criação do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ControleDeMedidas::Application.config.secret_token = '19817dd40c5f745ceee36b361ac778ff8936b9ddead8cc4c4251c001f94a9b967a7ae773b8f49071e90df628d564dfd98a712b1d496c865bf2ecf87e406524f5'
