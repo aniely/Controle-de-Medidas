b8bb7cf2db9aa5504b54c5f06a01756176bfa2b2
Aniely Martins <aniellymartins@gmail.com>
2012-02-15 21:20:19 -0200

    Gerenciar Pessoa - Alterar

--- spec/acceptance/gerenciar_pessoa_spec.rb
+++ spec/acceptance/gerenciar_pessoa_spec.rb
@@ -27,5 +27,26 @@ feature 'gerenciar pessoa' do
 
  end
 
+  scenario 'alterar pessoa' do #, :javascript => true do
+    pessoa = FactoryGirl.create(:pessoa)
+    visit edit_pessoa_path(pessoa)
+
+    fill_in "nome", :with => 'Aniely'
+    fill_in "sexo", :with => 'feminino'
+    fill_in "cpd", :with =>'21221221'
+    fill_in "telefone", :with => '1111111'
+    fill_in "datanascimento", :with => '12/11/1991'
+    fill_in "email", :with => 'aniely@mail.com'
+        
+    click_button 'Salvar'
+   
+    page.should have_content 'Nome: Aniely'
+    page.should have_content 'Sexo: feminino'
+    page.should have_content 'Cpd: 21221221'
+    page.should have_content 'Telefone: 1111111'
+    page.should have_content 'Datanascimento: 12/11/1991'
+    page.should have_content 'Email: aniely@mail.com'
+    
+  end
 end
 
