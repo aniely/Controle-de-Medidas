e0aed73d740b255dc89b68a108a2e85105825a92
Aniely Martins <aniellymartins@gmail.com>
2012-03-02 10:12:55 -0300

    Alterar Medida

--- spec/acceptance/gerenciar_medida_spec.rb
+++ spec/acceptance/gerenciar_medida_spec.rb
@@ -27,6 +27,34 @@ feature 'gerenciar medida' do
 
   end
 
+    scenario 'alterar medida' do #, :javascript => true do
+
+    pessoa = FactoryGirl.create(:pessoa,:nome => 'Aniely')
+    medida = FactoryGirl.create(:medida, :pessoa => pessoa)
+
+    visit edit_medida_path(medida)
+
+    preencher_e_verificar_medida
+
+  end
+
+  def preencher_e_verificar_medida
+
+    fill_in 'Peso', :with => '60'
+    fill_in 'Altura', :with => '1.70'
+    fill_in 'Medida', :with => '150'
+    select 'Aniely', :on => 'Nome'
+        
+    click_button 'Salvar'
+   
+   
+    page.should have_content 'Peso: 60'
+    page.should have_content 'Altura: 1.70'
+    page.should have_content 'Medida: 150'
+    page.should have_content 'Nome: Aniely'
+
+  end
+ 
  
 end
 
