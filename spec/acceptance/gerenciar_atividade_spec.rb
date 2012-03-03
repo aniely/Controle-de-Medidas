0110c489abffc89347c0e925d33e9e18ef601f6e
Aniely Martins <aniellymartins@gmail.com>
2012-03-03 09:28:47 -0300

    Alterar Atividade

--- spec/acceptance/gerenciar_atividade_spec.rb
+++ spec/acceptance/gerenciar_atividade_spec.rb
@@ -29,4 +29,37 @@ feature 'gerenciar atividade' do
     page.should have_content 'Nome: Aniely'
   end
 
+
+    scenario 'alterar atividade' do #, :javascript => true do
+
+    pessoa = FactoryGirl.create(:pessoa,:nome => 'Aniely')
+    atividade = FactoryGirl.create(:atividade, :pessoa => pessoa)
+
+    visit edit_atividade_path(atividade)
+
+    preencher_e_verificar_atividade
+
+  end
+
+  def preencher_e_verificar_atividade
+
+    fill_in 'Nomeat', :with => 'corrida'
+    fill_in 'Tipo', :with => 'aerobica'
+    fill_in 'Calorias', :with => '100'
+    fill_in 'Horas', :with => '1'
+    select  'Aniely', :on => 'Nome' 
+
+        
+    click_button 'Salvar'
+   
+   
+    page.should have_content 'Nomeat: corrida'
+    page.should have_content 'Tipo: aerobica'
+    page.should have_content 'Calorias: 100'
+    page.should have_content 'Horas: 1'
+    page.should have_content 'Nome: Aniely'
+
+
+  end
+
 end
