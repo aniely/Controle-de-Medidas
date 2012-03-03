f9587f043fc1375d29874a99316d54603f736c7d
Aniely Martins <aniellymartins@gmail.com>
2012-03-03 10:27:00 -0300

    Alterar Alimentacao

--- spec/acceptance/gerenciar_alimentacao_spec.rb
+++ spec/acceptance/gerenciar_alimentacao_spec.rb
@@ -24,4 +24,32 @@ feature 'gerenciar alimentacao' do
     page.should have_content 'Tipo: legumes'
     page.should have_content 'Nome: Aniely'
   end
+
+    scenario 'alterar alimentacao' do #, :javascript => true do
+
+    pessoa = FactoryGirl.create(:pessoa,:nome => 'Aniely')
+    alimentacao = FactoryGirl.create(:alimentacao, :pessoa => pessoa)
+
+    visit edit_alimentacao_path(alimentacao)
+
+    preencher_e_verificar_alimentacao
+
+  end
+  
+   
+    def preencher_e_verificar_alimentacao
+
+    fill_in 'Calorias', :with => '100'
+    fill_in 'Tipo', :with => 'legumes'
+    select  'Aniely', :on => 'Nome'         
+   
+    click_button 'Salvar'
+   
+   
+    page.should have_content 'Calorias: 100'
+    page.should have_content 'Tipo: legumes'
+    page.should have_content 'Nome: Aniely'
+
+  end
+
 end
